cb50c873f3519d5cad91a81a1d143ffe12bd83e9
JAVA <o165o.oppo@gmail.com>
2025-11-15 12:55:15 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Fade Up Animation */
+.fade-up {
+    opacity: 0;
+    transform: translateY(40px);
+    transition: 0.7s ease;
+}
+
+.fade-up.visible {
+    opacity: 1;
+    transform: translateY(0);
+}
